12bf58989d7ace11ff82da1e34323bbe45c40c18
Alperen Ekin <alperenekin97@gmail.com>
2020-11-14 16:02:18 +0300

    Basit map works

--- lib/google_maps/google_maps_view.dart
+++ lib/google_maps/google_maps_view.dart
@@ -7,20 +7,41 @@ class GoogleMapsView extends StatefulWidget {
 }
 
 class _GoogleMapsViewState extends State<GoogleMapsView> {
+  GoogleMapController controller;
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(40.9130116, 29.2094771),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.animateCamera(CameraUpdate.newLatLng(LatLng(40.9906621, 29.0200943)));
+        },
+      ),
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kLake,
-        onMapCreated: (map){},
+        onMapCreated: (map) {
+          controller = map;
+        },
+        markers: _createMarker(),
       ),
     );
+  }
+
+  Set<Marker> _createMarker() {
+    return <Marker>[
+      Marker(
+          markerId: MarkerId("asdad"),
+          position: _kLake.target,
+          zIndex: 10,
+          infoWindow: InfoWindow(title:"Hello"),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure))
+    ].toSet();
   }
 }
